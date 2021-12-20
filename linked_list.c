13e68e3ec90e2e342408e2348989c25eb655ff81
Vaibhav Choudhary <vaibhavch29@gmail.com>
2021-12-20 19:27:36 +0530

    testing

--- linked_list.c
+++ linked_list.c
@@ -7,6 +7,11 @@ typedef struct node
     struct node *next;
 }node;
 
+void Print_list(node *n);
+
+//Given a reference (pointer to pointer) for the head of the list
+void Insert_At_Beg(node **n, int data_input);
+
 int main()
 {
     node *head=NULL;
@@ -21,9 +26,54 @@ int main()
 
     head=n;
 
-    printf("First Node:%d",head->data);
+    //printf("First Node:%d",head->data);
+    Print_list(head);
+
+    int num;
+    printf("\nEnter the element:");
+    scanf("%d",&num);
+
+    Insert_At_Beg(&head,num);
+
+    
+
+    Print_list(head);
 
     free(head);
 
     return 0;
+}
+
+void Print_list(node *n)
+{
+    printf("\nPrinting List\n");
+    while(n!=NULL)
+    {
+        printf("%d\t",n->data);
+        n=n->next;
+    }
+
+}
+
+void Insert_At_Beg(node **n, int data_input)
+{
+    node *temp=(struct node*)malloc(sizeof(node));
+    temp->next=NULL;
+    
+
+    if(temp!=NULL)
+    {
+        temp->data=data_input;
+
+        if(n==NULL)
+        {
+            *n=temp; 
+        }else
+        {
+            temp->next=*n;
+            *n=temp;
+
+        }
+    }
+
 }
